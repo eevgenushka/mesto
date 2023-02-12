ca4d4bf8b27ae459598a00f8baa6020c627410d8
Evgenia Fedotova <Fedotovaevgenia1793@gmail.com>
2023-02-12 10:40:29 +0400

    "исправить после первой иттерации"

--- script/index.js
+++ script/index.js
@@ -1,15 +1,26 @@
 const profileEdit = document.querySelector('.profile__edit');
-
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+const popupForm = document.querySelector('.popup__form');
+const popupButtonClose = document.querySelector('.popup__button-close');
+const popupInputTypeName = document.querySelector('.popup__input_type_name');
+const popupInputTypeJob= document.querySelector('.popup__input_type_job');
+const popup = document.querySelector('.popup');
 
 profileEdit.addEventListener('click', function() {
-  const popup = document.querySelector('.popup');
+  popupInputTypeName.value = profileName.textContent;
+  popupInputTypeJob.value = profileAbout.textContent;
   popup.classList.add('popup_opened');
 });
 
-const popupButtonClose = document.querySelector('.popup__button-close');
-
-
 popupButtonClose.addEventListener('click', function() {
-  const popup = document.querySelector('.popup');
   popup.classList.remove('popup_opened');
 });
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  profileName.textContent = popupInputTypeName.value;
+  profileAbout.textContent = popupInputTypeJob.value;
+  popup.classList.remove('popup_opened');
+}
+popupForm.addEventListener('submit', handleFormSubmit);
